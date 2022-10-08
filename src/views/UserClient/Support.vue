d64764493ff787c826d6499a54f8f649f20c2c42
Victor Hugo <victor.hugo@spezi.com.br>
2022-10-08 14:51:00 -0300

    feature: telas finalizadas ainda não aprovadas.

--- src/views/UserClient/Support.vue
+++ src/views/UserClient/Support.vue
@@ -7,6 +7,103 @@
           Contato de suporte para ajuda via telefone.
         </v-card-subtitle>
         <v-divider class="mx-4"></v-divider>
+        <v-row
+          align="center"
+          justify="center"
+          :style="{ height: deviceHeight * 0.6 + 'px' }"
+        >
+          <!-- HEADSET -->
+          <v-col
+            cols="12"
+            xs="12"
+            sm="12"
+            md="12"
+            lg="6"
+            class="d-flex justify-center"
+          >
+            <div class="pt-3">
+              <v-sheet outlined color="primary" rounded>
+                <v-card class="pa-3" width="600" outlined elevation="3">
+                  <v-row
+                    align="center"
+                    justify="center"
+                    :style="{ height: deviceHeight * 0.32 + 'px' }"
+                  >
+                    <v-col class="fill-height" height="300">
+                      <v-card
+                        class="
+                          elevation-0
+                          text-center
+                          d-flex
+                          flex-column
+                          align-center
+                          justify-center
+                        "
+                        height="100%"
+                      >
+                        <div>
+                          <v-icon size="80" color="primary">mdi-headset</v-icon>
+                          <h1 class="mt-10">0800 770 2821</h1>
+                        </div>
+                      </v-card>
+                    </v-col>
+                  </v-row>
+                </v-card>
+              </v-sheet>
+            </div>
+          </v-col>
+
+          <!-- CHAT -->
+          <v-col
+            cols="12"
+            xs="12"
+            sm="12"
+            md="12"
+            lg="6"
+            class="d-flex justify-center"
+          >
+            <div class="pt-3">
+              <v-sheet outlined color="primary" rounded>
+                <v-card class="pa-3" width="600" oulined elevation="3">
+                  <v-row
+                    align="center"
+                    justify="center"
+                    :style="{ height: deviceHeight * 0.32 + 'px' }"
+                  >
+                    <v-col class="fill-height" height="300">
+                      <v-card
+                        class="
+                          elevation-0
+                          text-center
+                          d-flex
+                          flex-column
+                          align-center
+                          justify-center
+                        "
+                        height="100%"
+                      >
+                        <div>
+                          <v-icon size="80" color="primary">mdi-headset</v-icon>
+                          <h1 class="mt-10">0800 770 2821</h1>
+                        </div>
+                      </v-card>
+                    </v-col>
+                  </v-row>
+                </v-card>
+              </v-sheet>
+            </div>
+          </v-col>
+
+          <!-- DESCRIÇÃO -->
+        </v-row>
+        <v-row align="center" justify="center">
+          <b>
+            De segunda à sexta-feira, das 9h às 18h, mediante fila de
+            atendimento.E aos sábados, plantão emergencial para clientes, das
+            10h às 17h. <b class="description">Clique aqui</b> para enviar uma
+            solicitação por e-mail.
+          </b>
+        </v-row>
       </v-card>
     </v-col>
   </v-row>
@@ -17,13 +114,61 @@ import { mapState } from "vuex";
 
 export default {
   components: {},
-  data: () => ({}),
-  mounted() {},
-  methods: {},
+  data: () => ({
+    proccesses: [],
+    phases: [],
+    selectedItem: 1,
+    items: [
+      { text: "Real-Time" },
+      { text: "Audience" },
+      { text: "Conversions" },
+    ],
+  }),
+  mounted() {
+    this.getProccess();
+  },
+  methods: {
+    async getProccess() {
+      try {
+        const response = await this.$axios.get(
+          "proccesses/",
+          this.currentUser.id
+        );
+        this.proccesses = response.data;
+        if (response.data.phase) {
+          this.phases = response.data.phase.map((el) => {
+            return {
+              id: el.id,
+              description: el.description,
+              proccess_id: el.proccess_id,
+              created_at: this.formatDateTime(el.created_at),
+              updated_at: this.formatDateTime(el.updated_at),
+            };
+          });
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    formatDateTime(dataTime) {
+      const day = dataTime.slice(8, 10);
+      const month = dataTime.slice(5, 7);
+      const year = dataTime.slice(0, 4);
+      const time = dataTime.slice(11, 19);
+
+      return `${day}/${month}/${year} às ${time}`;
+    },
+  },
   computed: {
     ...mapState({
       currentUser: (state) => state.authentication.currentUser,
     }),
+    deviceHeight() {
+      return this.$vuetify.breakpoint.height;
+    },
+    deviceWidth() {
+      return this.$vuetify.breakpoint.width;
+    },
   },
 };
 </script>
@@ -31,5 +176,12 @@ export default {
 <style>
 .access {
   color: white;
+}
+.text {
+  font-size: 5vmin;
+}
+.description {
+  font-weight: bolder;
+  color: #7ed957;
 }
 </style>
---
+++ src/views/UserClient/test.vue
@@ -0,0 +1,46 @@
+        <v-row class="my-16" justify="center">
+          <v-col cols="6" class="d-flex justify-center">
+            <v-row
+              align="center"
+              justify="center"
+              :style="{ height: deviceHeight * 0.32 + 'px' }"
+            >
+              <v-card class="mt-10" height="100%" width="80%" outlined>
+                <v-col
+                  cols="12"
+                  md="12"
+                  sm="12"
+                  lg="12"
+                  class="d-flex justify-center fill-height"
+                >
+                  <v-icon icon size="100" color="primary"> mdi-headset </v-icon>
+                </v-col>
+              </v-card>
+            </v-row>
+          </v-col>
+          <v-col cols="6" class="d-flex justify-center">
+            <!-- align="center"
+            justify="center"
+            :style="{ height: deviceHeight * 0.32 + 'px' }" -->
+            <v-row
+            >
+              <v-card class="mt-10" height="100%" width="80%" outlined>
+                <v-row
+                  align="center"
+                  justify="center"
+                  :style="{ height: deviceHeight * 0.32 + 'px' }"
+                >
+                  <v-col
+                    cols="6"
+                    md="12"
+                    sm="12"
+                    lg="12"
+                    class="d-flex justify-center fill-height"
+                  >
+                    <v-icon icon size="100" color="primary"> mdi-forum </v-icon>
+                  </v-col>
+                </v-row>
+              </v-card>
+            </v-row>
+          </v-col>
+        </v-row>
